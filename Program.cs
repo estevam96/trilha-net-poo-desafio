5899f694d3c562e71871841d7d647f0f30ac0e53
Marcelo Estevam <marcelo.estevam@smartmresearch.com>
2024-01-12 17:00:11 -0300

    chore: resolve desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia:");
+Smartphone nokia = new Nokia(numero: "(11) 1111-1111", modelo: "C300", imei: "123456789", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular iPhone:");
+Smartphone iphone = new Iphone(numero: "(11) 1234-5678", modelo: "Iphone 15", imei: "987654321", memoria: 62);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
